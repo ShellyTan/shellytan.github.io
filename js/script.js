560b65bb648ef448f11fb44e048e8242f141b5bc
ShellyTan <shellytan2010@gmail.com>
2015-05-14 11:45:07 -0400

    Set up script to pull daily gif data from google spreadsheet. Deleted old html.

--- js/script.js
+++ js/script.js
@@ -8,19 +8,60 @@ var $back_to_top = $('.back-to-top');
 
 var section_index = [ $code_section , $graphic_section , $studio_section ];
 
-var on_page_scroll = function() {
+var on_page_scroll = function(){
 
     if(window.pageYOffset > 800) {
         $back_to_top.fadeIn(1000);
     } else {
         $back_to_top.fadeOut(1000);
     }
-};
+}
 
-var back_to_top = function() {
+var back_to_top = function(){
     $body.scrollTo($portfolio_nav, { duration:450 }, 'y');
     return false;
-};
+}
+
+var lazy_load = function(){
+	$('img.gifs').lazyload({
+	    threshold : 200
+	});
+}
+
+var table_top = function(){
+	Tabletop.init({ 
+		key: '1gh2al3t_pro4ejyWkZmEsJaQZWrUD06f21mXX0tTRpQ',
+		callback: function(data, tabletop) { 
+		    load_data(data);
+		},
+		simpleSheet: true 
+    });
+}
+
+var load_data = function(data){
+	var index = data.length; 
+
+	$.each(data, function(k,v) {
+		v.date_id = slugify(v.date);
+		var r = ich.gifstemplate(v);
+		$('#gifs-container').prepend(r);
+	});
+
+	timeout(index);
+}
+
+var timeout = function(index){
+	if( $('img.gifs').length === index ){
+		lazy_load();
+	}else{
+		console.log('not loaded');
+		setTimeout( timeout(index), 50 );
+	}
+}
+
+var slugify = function(txt){
+    return txt.replace(/\s/g,'-').replace(/,/g,'').toLowerCase();
+}
 
 $( document ).ready(function() {
 
@@ -51,7 +92,7 @@ $( document ).ready(function() {
 
 	$back_to_top.on('click', back_to_top);
 
-	$('img.gifs').lazyload({
-	    threshold : 200
-	});
+	ich.grabTemplates();
+	table_top();
+	
 });
