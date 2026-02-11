ac45f165dbc5820ff63a03fed9908b4cd0b15106
SHELLY TAN <shelly.tan@washpost.com>
2026-02-11 01:20:28 -0500

    Adding in draft of new name image. Adjusting styling and font sizing

--- shellytan-site/src/components/TitleScreen.js
+++ shellytan-site/src/components/TitleScreen.js
@@ -1,20 +1,24 @@
 // import logo from '../logo.svg';
 import '../styles/TitleScreen.scss';
+import name from '../name.png';
 
 function TitleScreen() {
   return (
-    <div className="title-screen">
+    <div id="title-screen">
       <div className="text">
-        <h3 className="top">Hi, my name is</h3>
-        <h1 className="block3d">Shelly</h1>
-        <h3>Designer, developer, illustrator and reporter</h3>
+        <h2 className="top">Hi, my name is</h2>
+        {/* <h1>Shelly</h1> */}
+        <div className="name-wrapper">
+          <img src={name} alt="Pixel art version of the name Shelly" />
+        </div>
+
+        <h2 className="bottom">Journalist, designer, developer</h2>
 
         <span className="nav about">About<span className="nav__gloss" aria-hidden="true">About</span></span>
         <span className="nav portfolio">Portfolio<span className="nav__gloss" aria-hidden="true">Portfolio</span></span>
         <span className="nav contact">Contact<span className="nav__gloss" aria-hidden="true">Contact</span></span>
-
       </div>
-      <div className="starry-sky">
+      <div id="starry-sky">
         <span className="star-layer--large" aria-hidden="true"></span>
       </div>
     </div>
