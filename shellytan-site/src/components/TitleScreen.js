7824fbfc24bd7d06e0a06583765edde78ef2d39a
SHELLY TAN <shelly.tan@washpost.com>
2026-02-12 21:42:58 -0500

    Cleaning up and organizing

--- shellytan-site/src/components/TitleScreen.js
+++ shellytan-site/src/components/TitleScreen.js
@@ -1,29 +1,24 @@
-// import logo from '../logo.svg';
-import '../styles/TitleScreen.scss';
 import name from '../img/name-2.png';
 
 function TitleScreen() {
   return (
-    <div className="container">
-      <div id="title-screen">
-        <h2 className="top">Hi, my name is</h2>
+    <>
+        <p className="intro top">Hi, my name is</p>
         <h1>Shelly</h1>
         <div className="name-wrapper">
-          <img src={name} alt="" />
+          <img src={name} alt="" aria-hidden="true" />
         </div>
-        <h2 className="bottom">Journalist, designer, developer</h2>
-      </div>
-      <nav aria-label="Homepage navigation">
-        <ul class="nav">
-          <li><a href="/about" class="btn-green">About</a></li>
-          <li><a href="/portfolio" class="btn-green">Portfolio</a></li>
-          <li><a href="/contact" class="btn-green">Contact</a></li>
-        </ul>
-      </nav>
-      <div id="starry-sky" aria-hidden="true">
-        <span className="star-layer--large" aria-hidden="true"></span>
-      </div>
-    </div>
+
+        <p className="intro bottom">Journalist, designer, developer</p>
+
+        <nav aria-label="Homepage button navigation">
+          <ul class="nav">
+            <li><a href="/about" className="btn-green">About</a></li>
+            <li><a href="/portfolio" className="btn-green">Portfolio</a></li>
+            <li><a href="/contact" className="btn-green">Contact</a></li>
+          </ul>
+        </nav>
+    </>
   );
 }
 
--- shellytan-site/src/index.js
+++ shellytan-site/src/index.js
@@ -7,7 +7,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TitleScreen />
+    <div className="container">
+      <header id="title-screen">
+        <TitleScreen />
+      </header>
+      <main>
+        <div id="starry-sky" aria-hidden="true">
+          <span className="star-layer--large"></span>
+        </div>
+      </main>
+    </div>
   </React.StrictMode>
 );
 
