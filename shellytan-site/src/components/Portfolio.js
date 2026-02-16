a4fc6f266452fb722fe8c3052b45ec6ac322d5a4
SHELLY TAN <shelly.tan@washpost.com>
2026-02-16 04:39:01 -0500

    Setting up basic structure for portfolio

---
+++ shellytan-site/src/components/Portfolio.js
@@ -0,0 +1,47 @@
+function Portfolio() {
+  return (
+    <div className="wrapper portfolio">
+        <div className="text">
+            <h2>Portfolio</h2>
+            <p className="body">Here are a few of my favorite projects. If you hit a paywall and are unable to view, please <a href="mailto:contact.shelly.tan@gmail.com" aria-label="Email">get in touch with me</a> and I can send you examples directly.</p>
+        </div>
+        <div className="grid">
+            <div>
+                <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                <img src="/portfolio/cgi-quiz.jpg" alt="" />
+                </a>
+                <p>
+                    <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                    Can you tell when you’re seeing CGI in movies and TV shows? 
+                    </a> 
+                    <span>Date X/X/2025</span>
+                </p>
+            </div>
+            <div>
+                <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                <img src="/portfolio/cgi-quiz.jpg" alt="" />
+                </a>
+                <p>
+                    <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                    Can you tell when you’re seeing CGI in movies and TV shows? 
+                    </a> 
+                    <span>Date X/X/2025</span>
+                </p>
+            </div>
+            <div>
+                <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                <img src="/portfolio/cgi-quiz.jpg" alt="" />
+                </a>
+                <p>
+                    <a href="https://www.washingtonpost.com/style/interactive/2025/wicked-severance-avatar-jurassic-world-rebirth-cgi-effects/" target="_blank" rel="noopener noreferrer">
+                    Can you tell when you’re seeing CGI in movies and TV shows? 
+                    </a> 
+                    <span>Date X/X/2025</span>
+                </p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Portfolio;
